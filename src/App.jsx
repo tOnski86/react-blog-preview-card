38c95275eddcbf8db0cfd99a7ef2568a2aacef6d
Antonio Saqueton <code.antoniohicetasaqueton@gmail.com>
2024-11-08 04:24:57 +0800

    Implement dateConverter function

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+const data = [
+  {
+    author: 'Greg Hooper',
+    authorImage: '/image-avatar.webp',
+    articleImage: '/illustration-article.svg',
+    category: 'learning',
+    date: '1703160060',
+    title: 'HTML and CSS foundations',
+    previewText:
+      'These languages are the backbone of every website, defining structure, content, and presentation.',
+  },
+];
+
 function App() {
-  console.log(new Date());
+  function dateConverter(timestamp) {
+    return new Date(+timestamp * 1000).toLocaleDateString('en-gb', {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+    });
+  }
+  // console.log(dateConverter(data[0].date));
+
   return (
     <div>
       <h1 className='text-gray-950'>Hello React!</h1>
